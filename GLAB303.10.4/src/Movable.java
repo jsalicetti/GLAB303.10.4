bea4661b903c78b900ff10efc03cd46ae90bf52f
jsalicetti <jessicasalicetti@yahoo.com>
2024-07-17 11:21:36 -0400

    completed

---
+++ GLAB303.10.4/src/Movable.java
@@ -0,0 +1,8 @@
+public interface Movable {
+    void moveUp();    // "public" and "abstract" by default
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+    String getCoordinate();
+}
+
